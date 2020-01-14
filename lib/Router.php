f16b95d119a01f8e277a5a7bfd23193d586263a4
AKozlova <a.kozlova@sa.media>
2020-01-14 19:12:19 +0300

    do routing

--- lib/Router.php
+++ lib/Router.php
@@ -2,6 +2,7 @@
 
 
 namespace lib;
+
 use app\controllers\IndexController;
 use lib\exceptions\FrameworkException;
 
@@ -52,6 +53,7 @@ class Router
      */
     public function route(): string
     {
+        header('Content-Type: text/plain');
         if (isset($_SERVER['REQUEST_URI'])) {
             $url = $this->url;
             $router = static::getInstance();
@@ -62,24 +64,24 @@ class Router
             // /news/13/
             // там ищет, а тут нет
 
-            for ($i = 0; $i < count($router->routes); $i++) {
-                preg_match($router->routes[$i]["regex"], $url, $matches);
-
-                if (preg_match($router->routes[$i]["regex"], $url)) {
+            foreach($router->routes as $route) {
+                if (preg_match($route["regex"], $url, $matches)) {
                     // TODO убрать потом
                     // если найден путь, показывает паттерн и что найдено
-                    echo $router->routes[$i]["regex"] . " => ";
+                    echo $route["regex"] . " => ";
                     print_r($matches);
                     echo " || ";
                     //
 
-                    $controllerName = 'app\\controllers\\'.$router->routes[$i]["controllerName"]."Controller";
-                    $controllerMethod = $router->routes[$i]["controllerMethod"];
+                    $controllerName = 'app\\controllers\\' . $route["controllerName"] . "Controller";
+                    $controllerMethod = $route["controllerMethod"];
 
                     $object = new $controllerName();
                     return $object->$controllerMethod();
                 }
             }
+
+            exit;
 
             // TODO убрать потом
             // если нет пути, показывает паттерн, для которого не найдено и сам путь
@@ -96,33 +98,34 @@ class Router
         }
     }
 
-        /**
-         * Превратить путь вместе с параметрами в regex
-         * @param $pattern
-         * @param $params
-         * @return bool|string
-         */
-        private function getRegex($pattern, $params){
+    /**
+     * Превратить путь вместе с параметрами в regex
+     * @param $pattern
+     * @param $params
+     * @return bool|string
+     */
+    private function getRegex($pattern, $params)
+    {
         if (preg_match('/[^-:\/_{}()a-zA-Z\d]/', $pattern)) {
             return false; // Неправильный паттерн пути
         }
 
         /**
          * Заменяем каждый параметр в паттерне на "(?<параметр>[правило_параметра]+)"
-         * если в regex будет <что-то>, то оно просто исчезает (???), поэтому < меняем на ?&amp;lt;
+         * если в regex будет <что-то>, то оно просто исчезает (???), поэтому < меняем на &amp;lt;
          * и потом переделываем обратно с помощью htmlspecialchars_decode в return
          */
         foreach ($params as $key => $value) {
             $pattern = preg_replace(
                 '/{' . $key . '}/',
-                '(?&amp;lt;' . $key . '>' . $value . ')',
+                '(?<' . $key . '>' . $value . ')',
                 $pattern
             );
         }
 
         /**
          * Превращаем в полноценный regex, преобразовывая всякие
-         * ?&amp;lt в нормальные символы
+         * &amp;lt в нормальные символы
          */
         return "@^" . htmlspecialchars_decode($pattern) . "$@D";
     }
@@ -133,7 +136,8 @@ class Router
      * @param $mask
      * @return array|bool
      */
-    private function getMaskArray($mask){
+    private function getMaskArray($mask)
+    {
         if (!preg_match('/^.+\..+$/', $mask)) {
             return false; // Неправильный паттерн маски
         }
